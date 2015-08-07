b0af5ee24a12c205414afc0dd3e15753e1440220
cablej <jackhcable@gmail.com>
2015-08-07 13:55:12 -0500

    Initial Commit

---
+++ Vehicle Manager/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Vehicle Manager
+//
+//  Created by Jack Cable on 8/7/15.
+//  Copyright © 2015 Jack Cable. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
